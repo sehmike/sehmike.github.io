5edb3a22af26b265169a7bbf526559a327beea6f
Michael Seh <mikejseh@gmail.com>
2024-01-19 02:26:23 -0800

    final

--- script.js
+++
@@ -1,8 +0,0 @@
-const toggler = document.querySelector('.toggler')
-
-const circle = document.querySelector('.circle')
-
-toggler.addEventListener('click', () => {
-	circle.classList.toggle('clicked')
-	toggler.classList.toggle('dark')
-})
