fb7ab66d4d9b3c9a4681234bed2908ff2434cdc9
Michael Seh <mikejseh@gmail.com>
2024-02-17 19:33:50 -0800

    test

--- qNhXJKLikj7duaiYVfFQxujR3sHYWRZWnWnJFK9yBAi7E77.js
+++ qNhXJKLikj7duaiYVfFQxujR3sHYWRZWnWnJFK9yBAi7E77.js
@@ -1,1 +1,17 @@
-$(window).scroll((function(){var o=$(window),t=$("body"),n=$(".panel"),i=o.scrollTop()+o.height()/1.2;n.each((function(){var o=$(this);o.position().top<=i&&o.position().top+o.height()>i&&(t.removeClass((function(o,t){return(t.match(/(^|\s)color-\S+/g)||[]).join(" ")})),t.addClass("color-"+$(this).data("color")))}))})).scroll(),$(".content__article__link").each((function(){$(this).html();$(this).contents().wrap('<a href="https://yeezy.com/"></a>')}));
+let i = fi = z = 0
+    const qa = e => document.querySelectorAll(e),
+      a = qa("article"),
+      fs = qa("section p"),
+      f = e => e.style.zIndex = z,
+      ns = () => {
+        fs[fi].style.display = "none", fi = (fi + 1) % fs.length, fs[fi].style.display = "inline"
+      },
+      next = () => {
+        i = (i + 1) % a.length, z += 1
+        f(a[i]), ns()
+      },
+      click = () => {
+        next(), clearInterval(s)
+      }
+    document.body.onclick = click
+    const s = setInterval(  next, 1500)
