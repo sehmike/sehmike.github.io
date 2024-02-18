a63ef28d0fbee6f85a37c4b9333b85ad5095ea53
Michael Seh <mikejseh@gmail.com>
2024-02-17 19:42:02 -0800

    index

--- qNhXJKLikj7duaiYVfFQxujR3sHYWRZWnWnJFK9yBAi7E77.js
+++ qNhXJKLikj7duaiYVfFQxujR3sHYWRZWnWnJFK9yBAi7E77.js
@@ -1,17 +1,1 @@
-let i = fi = z = 0
-    const qa = e => document.querySelectorAll(e),
-      a = qa("article"),
-      fs = qa("section p"),
-      f = e => e.style.zIndex = z,
-      ns = () => {
-        fs[fi].style.display = "none", fi = (fi + 1) % fs.length, fs[fi].style.display = "inline"
-      },
-      next = () => {
-        i = (i + 1) % a.length, z += 1
-        f(a[i]), ns()
-      },
-      click = () => {
-        next(), clearInterval(s)
-      }
-    document.body.onclick = click
-    const s = setInterval(  next, 1500)
+let i=fi=z=0;const qa=e=>document.querySelectorAll(e),a=qa("article"),fs=qa("section p"),f=e=>e.style.zIndex=z,ns=()=>{fs[fi].style.display="none",fi=(fi+1)%fs.length,fs[fi].style.display="inline"},next=()=>{i=(i+1)%a.length,z+=1;f(a[i]),ns()},click=()=>{next(),clearInterval(s)}document.body.onclick=click;const s=setInterval(next,1500);
