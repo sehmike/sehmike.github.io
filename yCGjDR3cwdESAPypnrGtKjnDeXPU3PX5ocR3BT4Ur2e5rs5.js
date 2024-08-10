dc7f0e0aba635b06ef31bb7abacbf733b60e067b
Michael Seh <mikejseh@gmail.com>
2024-08-09 22:45:54 -0700

    update content

---
+++ yCGjDR3cwdESAPypnrGtKjnDeXPU3PX5ocR3BT4Ur2e5rs5.js
@@ -0,0 +1,19 @@
+const baseUrl = "https://michaelseh.com/album/";
+const images = [];
+const start = 1;
+const end = 1052;
+
+for (let i = start; i <= end; i++) {
+  images.push(`${baseUrl}${i}.jpg`);
+}
+
+const imageDisplay = document.getElementById("image-display");
+
+function shuffleImage() {
+  const randomIndex = Math.floor(Math.random() * images.length);
+  imageDisplay.src = images[randomIndex];
+}
+
+shuffleImage();
+
+setInterval(shuffleImage, 500);
