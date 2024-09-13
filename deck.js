62bc48f435ddffe39010e8fb02d7f7df14f81a4e
Michael Seh <mikejseh@gmail.com>
2024-09-12 21:37:59 -0700

    sample of deck

--- deck.js
+++ deck.js
@@ -1,17 +1,1 @@
-const images = document.querySelectorAll('.slideshow img');
-let currentIndex = 0;
-
-function showImage(index) {
-  images.forEach(img => img.style.display = 'none');
-  images[index].style.display = 'block';
-}
-
-function nextImage() {
-  currentIndex = (currentIndex + 1) % images.length;
-  showImage(currentIndex);
-}
-
-setInterval(nextImage, 500);
-
-// Show the first image initially
-showImage(currentIndex);
+const images=document.querySelectorAll(".slideshow img");let currentIndex=0;function showImage(e){images.forEach((e=>e.style.display="none")),images[e].style.display="block"}function nextImage(){currentIndex=(currentIndex+1)%images.length,showImage(currentIndex)}setInterval(nextImage,500),showImage(currentIndex);
