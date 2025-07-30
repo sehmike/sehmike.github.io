19ae0e1aae59ac6c57e84806c14f61b141b1e8fc
Michael Seh <mikejseh@gmail.com>
2025-07-29 23:53:27 -0700

    mix

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 window.addEventListener('load', function() {
-  const fullBgUrl = "https://michaelseh.com/001.gif?" + Math.random();
-
-  const img = new Image();
-  img.src = fullBgUrl;
-
-  img.onload = function() {
-    document.documentElement.style.backgroundImage = `url('${fullBgUrl}')`;
-    document.documentElement.style.backgroundRepeat = "no-repeat";
-    document.documentElement.style.backgroundPosition = "center center";
-    document.documentElement.style.backgroundAttachment = "fixed";
-    document.documentElement.style.backgroundSize = "cover";
-  };
-});
+    const fullBgUrl = "https://michaelseh.com/001.gif?" + Math.random();
+    const bgGif = document.getElementById('bg-gif');
+    const img = new Image();
+    img.src = fullBgUrl;
+    img.onload = function() {
+      bgGif.src = fullBgUrl;
+    };
+  });
