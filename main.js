29e7bc3f849ac5a82190393335c92e4e1ba850af
Michael Seh <mikejseh@gmail.com>
2025-07-30 00:06:13 -0700

    001

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 window.addEventListener('load', function() {
+  // Animated GIF URL with cache buster to avoid stale cached static frames
   const gifUrl = "https://michaelseh.com/001.gif?" + Math.random();
 
   const bgGif = document.getElementById('bg-gif');
 
+  // Preload the GIF
   const preloadImg = new Image();
   preloadImg.src = gifUrl;
 
   preloadImg.onload = function() {
+    // Once fully loaded, replace the PNG with the animated GIF
     bgGif.src = gifUrl;
   };
 });
