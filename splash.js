378c75e050166ead0d2c6bba30f64ef96cd38418
Michael Seh <mikejseh@gmail.com>
2025-08-18 08:31:23 -0700

    splash.js

---
+++ splash.js
@@ -0,0 +1,8 @@
+<script>
+  window.addEventListener('load', function() {
+    const splash = document.querySelector('.fullscreen, .splash');
+    if (splash) {
+      splash.style.display = 'none';
+    }
+  });
+</script>
