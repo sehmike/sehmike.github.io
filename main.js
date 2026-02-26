fff19abe951a698c84461cd71d9d30063262b33e
Michael Seh <mikejseh@gmail.com>
2026-02-26 00:40:31 -0800

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const experiments = [
   {
     type: 'video',
     src: 'https://michaelseh.com/videos/Emoji-motion-cues.mp4',
-    caption: 'Emoji motion cues. <span class="muted">Vehicle Motion Cues is a feature that displays small dots along your screen edges while riding in a car. The dots shift with vehicle movement, giving your eyes a reference point to reduce motion sickness. This experiment swaps the dots for emoji as a hidden Easter egg. Steering wheels, traffic lights, pedestrians reacting to every turn and brake.</span>'
+    caption: 'Emoji motion cues. <span class="muted">Vehicle motion cues is a feature that displays small dots along your screen edges while riding in a car. The dots shift with vehicle movement, giving your eyes a reference point to reduce motion sickness. This experiment swaps the dots for emoji as a hidden Easter egg. Steering wheels, traffic lights, pedestrians reacting to every turn and brake.</span>'
   }
 ];
 
